dd3e0f59a352cf39c06a4d65c90d9c9d87134015
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-04-03 18:05:44 -0500

    debug copyFile

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -20,7 +20,7 @@ const writeFile = fileContent => {
 
 const copyFile = () => {
     return new Promise((resolve, reject) => {
-        fs.copyFile('./src/style.css', fileContent, err => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
           // if there is an error, reject the Promise and send error to the Promise's `.catch()` method
           if (err) {
               reject(err);
